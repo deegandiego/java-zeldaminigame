3d9e0878d30c02ec321cee610e1522c6a7dfd127
Diego Costa <dgcosta101@gmail.com>
2024-04-09 10:32:49 -0300

    Create Game class using JFrame

---
+++ src/zeldaminigame/Game.java
@@ -0,0 +1,31 @@
+package zeldaminigame;
+
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
+    public static int WIDTH = 480, HEIGHT = 480;
+
+    public Game() {
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    }
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        JFrame jf = new JFrame();
+
+        jf.add(game);
+        jf.setTitle("Mini Zelda");
+        jf.pack();
+        jf.setLocationRelativeTo(null);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setVisible(true);
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+    }
+}
