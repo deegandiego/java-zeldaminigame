8d25f275d9ca11244c4be1abf0f1988df94c6763
Diego Costa <dgcosta101@gmail.com>
2024-04-09 11:14:52 -0300

    Add render method

--- src/zeldaminigame/Game.java
+++ src/zeldaminigame/Game.java
@@ -1,7 +1,10 @@
 package zeldaminigame;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;
 
@@ -26,10 +29,38 @@ public class Game extends Canvas implements Runnable {
         new Thread(game).start();
     }
 
+    // Método responsável pela renderização gráfica do jogo
+    public void render() {
+        BufferStrategy bs = this.getBufferStrategy();
+
+        if (bs == null) {
+            this.createBufferStrategy(3);
+            return;
+        }
+
+        Graphics g = bs.getDrawGraphics();
+
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+
+        g.setColor(Color.RED);
+        g.fillRect(0, 0, 50, 50);
+
+        bs.show();
+    }
+
     @Override
     public void run() {
-        while(true) {
+        while (true) {
             System.out.println("Chamando game looping.");
+            render();
+
+            try {
+                // Limitando a 60 FPS
+                Thread.sleep(1000 / 60);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
